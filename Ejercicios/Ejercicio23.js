c6864e142c2b77d6e739fcbe5005b069bbfb7501
Stefie-fa <fandinostefie@gmail.com>
2023-12-25 00:17:36 +0100

    feat(ejercicios): respuesta  ejercicios 23 JS

--- Ejercicios/Ejercicio23.js
+++ Ejercicios/Ejercicio23.js
@@ -7,36 +7,34 @@ arrayLeaders([1,2,3,4,0]), [4]);
 */
 
 //function to find all the LEADERS in the array
-function arrayLeaders(numbers){
-
-  //Initialize the array to store leaders
+function arrayLeaders(numbers) {
+  // Initialize the array to store leaders
   let leaders = [];
-  let length = numbers.length - 1;
+  let length = numbers.length;
 
-  //Iterate through the array
-    for (let i = 0; i <= length; i++){
-      let currentElement = numbers[i];
-      let isLeader = true;
+  // Iterate through the array
+  for (let i = 0; i < length; i++) {
+    let currentElement = numbers[i];
+    let isLeader = true;
 
-      //check if the current element is greater than the sum of elements to its right
-      let sumRightNumbers = 0;
-      for (let j = i + 1; j <= length; j++) {
-        sumRightNumbers += numbers[j]; 
-
-        if (currentElement <= sumRightNumbers){
-          isLeader = false;
-          break;
-        }
-      }
-
-      if (isLeader) {
-        if (i !== length){
-          leaders.push(currentElement);
-        }    
-      }
+    // Check if the current element is greater than the sum of elements to its right
+    let sumRightNumbers = 0;
+    for (let j = i + 1; j < length; j++) {
+      sumRightNumbers += numbers[j];
     }
-    return leaders;
+
+    if (currentElement <= sumRightNumbers) {
+      isLeader = false;
+    }
+
+    // If it's the last element and greater than or equal to 0, or a leader, add it to the array
+    if ((i === length - 1 && currentElement > 0) || isLeader) {
+      leaders.push(currentElement);
+    }
   }
+
+  return leaders;
+}
 
   //call the function
   let result = arrayLeaders([1,2,3,4,0]);
