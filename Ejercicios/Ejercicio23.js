eb118d97106e8076f88b0de5f591a9674b81aac4
Stefie-fa <fandinostefie@gmail.com>
2023-12-24 23:38:06 +0100

    feat(ejercicios): respuesta  ejercicios 23 JS

--- Ejercicios/Ejercicio23.js
+++ Ejercicios/Ejercicio23.js
@@ -5,12 +5,41 @@
 arrayLeaders([1,2,3,4,0]), [4]);
 
 */
+
+//function to find all the LEADERS in the array
 function arrayLeaders(numbers){
-    return [];
+
+  //Initialize the array to store leaders
+  let leaders = [];
+  let length = numbers.length - 1;
+
+  //Iterate through the array
+    for (let i = 0; i <= length; i++){
+      let currentElement = numbers[i];
+      let isLeader = true;
+
+      //check if the current element is greater than the sum of elements to its right
+      let sumRightNumbers = 0;
+      for (let j = i + 1; j <= length; j++) {
+        sumRightNumbers += numbers[j]; 
+
+        if (currentElement <= sumRightNumbers){
+          isLeader = false;
+          break;
+        }
+      }
+
+      if (isLeader) {
+        if (i !== length){
+          leaders.push(currentElement);
+        }    
+      }
+    }
+    return leaders;
   }
 
-//var numero
-// creo array con el resto
-// sumar resto del array
-// bucle imprimir solo mayores que numero 
-//arrayLeaders([1,2,3,4,0]), [4]);
+  //call the function
+  let result = arrayLeaders([1,2,3,4,0]);
+
+  //display 
+  document.write(result);
