3e85974d0d5bfd07557d614c1a2e9130b76f29bb
Stefie-fa <fandinostefie@gmail.com>
2023-12-25 01:26:18 +0100

    feat(ejercicios): respuesta ejercicios 25 JS

--- Ejercicios/Ejercicio25.js
+++ Ejercicios/Ejercicio25.js
@@ -1,5 +1,17 @@
 "use strict";
 
 /*
-25
+25 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
+
+//Create the function
+function evenOrOdd(number) {
+    return number % 2 == 0 ? "Even" : "Odd";
+  }
+
+//Call the function
+let number = 6;
+let result = evenOrOdd(number);
+
+//Display the result
+document.write("The number " + number +  " is " + result);
