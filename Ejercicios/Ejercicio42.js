af1025b3eff2035292ffe8619c4db01e1ea263bc
Stefie-fa <fandinostefie@gmail.com>
2023-12-25 20:22:30 +0100

    feat(ejercicios): respuestas  ejercicios 42-45 JS

--- Ejercicios/Ejercicio42.js
+++ Ejercicios/Ejercicio42.js
@@ -1,6 +1,28 @@
 "use strict";
 
 /*
-42
+42 Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
 */
 
+function humanReadable(seconds) {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds % 3600) / 60);
+    let secondsExtra = seconds % 60;
+  
+    // Formatear los valores para asegurar que tengan dos dígitos
+    let formattedHours = hours < 10 ? '0' + hours : hours.toString();
+    let formattedMinutes = minutes < 10 ? '0' + minutes : minutes.toString();
+    let formattedSeconds = secondsExtra < 10 ? '0' + secondsExtra : secondsExtra.toString();
+  
+    // Devolver la cadena formateada
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  }
+  
+  console.log(humanReadable(59)); 
+  
--- Ejercicios/Ejercicio43.js
+++ Ejercicios/Ejercicio43.js
@@ -1,6 +1,25 @@
 "use strict";
 
 /*
-43
+43 Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
+function moveZeros(arr) {
+    // Create an empty array named zeros to store all the zeros found in the original array
+    let zeros = [];
+    
+    //filter out zeros and push the to zero array
+    arr = arr.filter(item => (item !== 0 ? true : zeros.push(item)&& false));
+    
+    // Concatenate the zeros array at the end of arr
+    return arr.concat(zeros);
+}
+
+// call the function
+let myArray = [1, 2, 0, 3, 0, 4, 5, 0, 6];
+let resultArray = moveZeros(myArray);
+
+// Display results
+console.log(resultArray);
